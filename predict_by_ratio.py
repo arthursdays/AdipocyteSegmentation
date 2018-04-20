b5ff949d79d3e91d0c050af50216ce422bac427f
KenCheong <14225077@life.hkbu.edu.hk>
2018-04-20 18:22:17 +0800

    Add files via upload

--- predict_by_ratio.py
+++ predict_by_ratio.py
@@ -3,13 +3,13 @@ from sklearn.metrics import *
 fp=open('Steatosis_stage(public).csv')
 stat=[]
 for line in fp.readlines()[1:-1]:
-    idd,stage,count,total_area,slice_area,nn,ratio,_=line.split(',')
+    idd,stage,count,total_area,slice_area,nn,ratio=line.split(',')
     stat.append([idd,int(stage),count,float(total_area),float(slice_area),nn,float(ratio)])
 
 
 def predict(v):
 #    return 1
-    if v>4.0 and v<12:
+    if v>7.0 and v<12:
         return 2
     elif v>12.0:
         return 3
@@ -19,7 +19,7 @@ def predict(v):
 def evaluate_error(data,stages):
     predict_labels=[predict(x) for x in data]
     accuracy = 1-zero_one_loss(predict_labels, stages)
-    print accuracy
+    print (accuracy)
 evaluate_error([x[-1] for x in stat],[x[1] for x in stat])
 
 
--- visualize.py
+++ visualize.py
@@ -3,25 +3,34 @@ from sklearn.metrics import *
 
 import matplotlib.pyplot as plt
 stat=[]
+fp=open('Steatosis_stage(public).csv')
 for line in fp.readlines()[1:-1]:
-    idd,stage,count,total_area,slice_area,nn,ratio,_=line.split(',')
+    print(line.split(','))
+    idd,stage,count,total_area,slice_area,nn,ratio,_,_,_=line.split(',')
     stat.append([idd,int(stage),count,float(total_area),float(slice_area),nn,float(ratio)])
 #print stat
 box_data=[0,0,0]
 count_data=[0,0,0]
 plot_data=[[],[],[]]
+plot_data_count=[[],[],[]]
 for i in stat:
     box_data[i[1]-1]+=i[-1]
     count_data[i[1]-1]+=1
-    print i[-1]
+    print(i[-1])
     plot_data[i[1]-1].append(i[-1])
+    plot_data_count[i[1]-1].append(i[-2])
 
 for i in range(len(box_data)):
     box_data[i]/=count_data[i]
 #plt.bar([1,2,3],box_data)
+'''
 plt.plot(plot_data[0],color='blue',label='1',marker='o')
 plt.plot(plot_data[1],color='yellow',label='2',marker='o')
 plt.plot(plot_data[2],color='red',label='3',marker='o')
+'''
+plt.scatter(plot_data[0],plot_data_count[0],color='blue',label='1',marker='o')
+plt.scatter(plot_data[1],plot_data_count[1],color='yellow',label='2',marker='o')
+plt.scatter(plot_data[2],plot_data_count[2],color='red',label='3',marker='o')
 plt.legend()
 plt.show()
 
