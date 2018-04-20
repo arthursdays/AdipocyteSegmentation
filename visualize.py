19ed9487e9f2f0121301bf584544efc687bba92e
KenCheong <14225077@life.hkbu.edu.hk>
2018-04-20 17:32:15 +0800

    Add files via upload

---
+++ visualize.py
@@ -0,0 +1,31 @@
+import numpy as np 
+from sklearn.metrics import *
+
+import matplotlib.pyplot as plt
+stat=[]
+for line in fp.readlines()[1:-1]:
+    idd,stage,count,total_area,slice_area,nn,ratio,_=line.split(',')
+    stat.append([idd,int(stage),count,float(total_area),float(slice_area),nn,float(ratio)])
+#print stat
+box_data=[0,0,0]
+count_data=[0,0,0]
+plot_data=[[],[],[]]
+for i in stat:
+    box_data[i[1]-1]+=i[-1]
+    count_data[i[1]-1]+=1
+    print i[-1]
+    plot_data[i[1]-1].append(i[-1])
+
+for i in range(len(box_data)):
+    box_data[i]/=count_data[i]
+#plt.bar([1,2,3],box_data)
+plt.plot(plot_data[0],color='blue',label='1',marker='o')
+plt.plot(plot_data[1],color='yellow',label='2',marker='o')
+plt.plot(plot_data[2],color='red',label='3',marker='o')
+plt.legend()
+plt.show()
+
+
+
+
+
